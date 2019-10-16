68b791c91e5647e05a5f5b94c8f983b1bdb6e1b4
git@github.com:sindhu211/AprSeleniumSessions.git <mgova@LAPTOP-BH638UES>
2019-10-16 18:58:09 +0530

    first commit

---
+++ sindhu/src/main/java/sindhu1/sindhu/App.java
@@ -0,0 +1,13 @@
+package sindhu1.sindhu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
